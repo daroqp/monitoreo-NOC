e0fd68311e7baed2987c7bf1648b6a270e54babb
Dario Quispe <daro.qp@gmail.com>
2024-08-27 19:56:27 -0300

    refactor: change url to call

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -9,7 +9,8 @@ export class Server {
 			'*/5 * * * * *',
 			() => {
 				// new CheckService().execute('https://google.com.ar');
-				const url = 'http://localhost:3000';
+				// const url = 'http://localhost:3000';
+				const url = 'http://google.com.ar';
 				new CheckService(
 					() => console.log(`${url} is ok`),
 					(error) => console.log(error)
