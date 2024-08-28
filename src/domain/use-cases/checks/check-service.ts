8c830aa77e1976265ee7e2b756898220fbafc952
Dario Quispe <daro.qp@gmail.com>
2024-08-28 01:43:40 -0300

    feat: add log repository implementation

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,5 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
 
 interface CheckServiceUseCase {
 	execute(url: string):Promise<boolean>;
@@ -9,6 +11,7 @@ type ErrorCallback = (error: string) => void;
 export class CheckService implements CheckServiceUseCase {
 
 	constructor(
+		private readonly logRepository: LogRepository,
 		private readonly successCallback: SuccessCallback,
 		private readonly errorCallback: ErrorCallback
 	){}
@@ -22,14 +25,19 @@ export class CheckService implements CheckServiceUseCase {
 				throw new Error(`Error on checkService ${url}`)
 			}
 
+			const log = new LogEntity(`Service ${url} working.`, LogSeverityLevel.low);
+			this.logRepository.saveLog(log);
 			this.successCallback();
 			
 			return true;
 
 		} catch (error) {
 			
-			this.errorCallback(`${error}`);
-			console.log(`${error}`)
+			const errorMessage = `${ error }`;
+			const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+			this. logRepository.saveLog(log);
+
+			this.errorCallback(errorMessage);
 			
 			return false	
 		}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infra/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infra/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service"
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+	new FileSystemDatasource(),
+)
 
 export class Server {
 	public static start(){
@@ -12,6 +18,7 @@ export class Server {
 				// const url = 'http://localhost:3000';
 				const url = 'http://google.com.ar';
 				new CheckService(
+					fileSystemLogRepository,
 					() => console.log(`${url} is ok`),
 					(error) => console.log(error)
 					).execute(url);
